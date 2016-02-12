5417db121757223c9851e662a7b5e3da61d4aa11
Nils Dannemann <ndannemann@gmail.com>
2016-02-12 15:06:31 +0100

    [add] side drawers [update] charts

--- client/components/chart/chart.js
+++ client/components/chart/chart.js
@@ -1,15 +1,26 @@
 Template.chart.onRendered(function () {
 	var data = {
-	  // A labels array that can contain any sort of values
-	  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-	  // Our series array that contains series objects or in this case series data arrays
+	  labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
 	  series: [
-	    [5, 2, 4, 2, 0]
+	    [5, 4, 3, 7, 5, 10, 3, 4, 8, 10, 6, 8],
+	    [3, 2, 9, 5, 4, 6, 4, 6, 7, 8, 7, 4]
 	  ]
 	};
 
-	// Create a new line chart object where as first parameter we pass in a selector
-	// that is resolving to our chart container element. The Second parameter
-	// is the actual data object.
-	new Chartist.Line('.ct-chart', data);
+	var options = {
+	  seriesBarDistance: 10
+	};
+
+	var responsiveOptions = [
+	  ['screen and (max-width: 650px)', {
+	    seriesBarDistance: 5,
+	    axisX: {
+	      labelInterpolationFnc: function (value) {
+	        return value[0];
+	      }
+	    }
+	  }]
+	];
+
+	new Chartist.Bar('.ct-chart', data, options, responsiveOptions);
 });
