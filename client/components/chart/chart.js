4504e93972402d1121ab55f1b542c833f0f905cb
Nils Dannemann <ndannemann@gmail.com>
2016-02-12 16:22:12 +0100

    [update] general layout + icons

--- client/components/chart/chart.js
+++ client/components/chart/chart.js
@@ -3,12 +3,13 @@ Template.chart.onRendered(function () {
 	  labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
 	  series: [
 	    [5, 4, 3, 7, 5, 10, 3, 4, 8, 10, 6, 8],
-	    [3, 2, 9, 5, 4, 6, 4, 6, 7, 8, 7, 4]
+	    [3, 2, 9, 5, 4, 6, 4, 6, 7, 8, 7, 4],
+	    [2, 3, 2, 5, 6, 8, 8, 7, 10, 9, 11, 11]
 	  ]
 	};
 
 	var options = {
-	  seriesBarDistance: 10
+	  seriesBarDistance: 12
 	};
 
 	var responsiveOptions = [
