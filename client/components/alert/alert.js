1fa4ef8fad5cad4f1908b68358868063d16d8839
Nils Dannemann <ndannemann@gmail.com>
2016-02-12 18:09:29 +0100

    [add] datatables

---
+++ client/components/alert/alert.js
@@ -0,0 +1,26 @@
+Meteor.startup(function () {
+	sAlert.config({
+		effect: 'slide',
+		sAlertIcon: 'asterisk',
+		position: 'bottom-right',
+		timeout: 2500,
+		html: true,
+		onRouteClose: false,
+		stack: true,
+		offset: 75,
+		stack: {
+		    spacing: 15, // in px
+		    limit: 3
+		},
+		beep: false,
+		onClose: _.noop
+	});
+});
+
+// Sample Alert
+Template.navigation.events({
+	'click .navigation--icons': function() {
+		// sAlert.info({sAlertIcon: 'ion-information-circled', message: 'Hi there! I\'m an info notification :-)'});
+		sAlert.success({sAlertIcon: 'ion-checkmark-circled', message: 'Hi there! I\'m a success notification :-)'});
+	}
+});
---
+++ client/components/table/table.js
@@ -0,0 +1,1 @@
+// Code in common.js
